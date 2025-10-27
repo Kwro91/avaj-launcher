129587cb438bfae1d7f82cf35e9325dbebf0b7e4
besalort <benjamin.salort@hotmail.com>
2025-10-27 17:40:32 +0100

    on peux maintenant lire un file

--- src/Start.java
+++ src/Start.java
@@ -1,13 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
 public class Start{
 	public static void main(String[] arg) {
 		System.out.printf("Hello World!\n");
-		test.ok();
-	}
-}
-
-class test {
-	static void ok(){
-		System.out.printf("OK test\n");
+		Vector<String> tab = ReadFile.readFile("scenario.txt");
+		int repet = Integer.parseInt(tab.elementAt(0));
+		System.out.println("////////////////////////////");
+		System.out.println(repet + " repetitions pour le programme");
 	}
 }
 
@@ -20,6 +22,12 @@ class test {
 
  // Projet
 
+ class BadValueException extends Exception {
+	public BadValueException(String message){
+		super(message);
+	}
+ }
+
  class Coordinates {
 	private int _longitude;
 	private int _latitude;
@@ -31,12 +39,42 @@ class test {
 		_height = 0;
 	}
 
+	Coordinates(int longitude, int latitude, int height) {
+		_longitude = 0;
+		_latitude = 0;
+		_height = 0;
+		try {
+			setHeight(height);
+			setLatitude(latitude);
+			setLongitude(longitude);
+		} catch (Exception err)
+		{
+			System.out.println(err.getMessage());
+		}
+	}
+
 	public int		getLongitude() {return (_longitude);}
 	public int		getLatitude() {return (_latitude);}
 	public int		getHeight() {return (_height);}
-	void	setLongitude(int longitude) {_longitude = longitude;}
-	void	setLatitude(int latitude) {_latitude = latitude;}
-	void	setHeight(int height) {_height = height;}
+	void	setLongitude(int longitude) throws BadValueException {
+		if (longitude >= 0)
+			_longitude = longitude;
+		else
+			throw new BadValueException("Error: invalid longitude value");
+	}
+	void	setLatitude(int latitude) throws BadValueException {
+		if (latitude >= 0)
+			_latitude = latitude;
+		else
+			throw new BadValueException("Error: invalid latitude value");
+	}
+	void	setHeight(int height) throws BadValueException {
+		if (height >= 0 && height <= 100)
+			_height = height;
+		else
+			throw new BadValueException("Error: invalid height value");
+
+	}
 }
 
 class WeatherTower {
@@ -64,13 +102,20 @@ abstract class Flyable {
 	}
 }
 
+class Aircraft {
+	long _id;
+	String _name;
+	Coordinates _coordinate;
+	Aircraft(long p_id, String p_name, Coordinates p_coordinates){}
+}
+
 
 // singletons (classe avec une seule instance)
 
 class WeatherProvider {
 	private static WeatherProvider instance = null;
 
-	private String weather[] = {"SUN", "RAIN", "SNOW"};
+	private String weather[] = {"RAIN", "FOG", "SUN", "SNOW"};
 
 	private WeatherProvider() {}
 
@@ -82,5 +127,25 @@ class WeatherProvider {
 
 	public String getCurrentWeather(Coordinates p_coordinates) {
 		return (weather[0]);
+	}
+}
+
+class ReadFile {
+	public  static Vector<String> readFile(String nameFile)
+	{
+		File file = new File(nameFile);
+		Vector<String> tab = new Vector<String>();
+		try (Scanner reader = new Scanner(file)){
+			while (reader.hasNextLine())
+			{
+				String data = reader.nextLine();
+				tab.add(data);
+				// System.out.println(data); //print tout le fichier
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: readfile");
+			e.printStackTrace();
+		}
+		return (tab);
 	}
 }
