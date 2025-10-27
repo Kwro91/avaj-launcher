9fcbe482e63290214e9f3e7d8afccba2280700a2
besalort <benjamin.salort@hotmail.com>
2025-10-27 19:15:30 +0100

    clean du code en cours

--- src/avaj/Start.java
+++ src/avaj/Start.java
@@ -21,64 +21,9 @@ public class Start{
 
  // Projet
 
- class BadValueException extends Exception {
-	public BadValueException(String message){
-		super(message);
-	}
- }
-
- class Coordinates {
-	private int _longitude;
-	private int _latitude;
-	private int _height;
-
-	Coordinates(){
-		_longitude = 0;
-		_latitude = 0;
-		_height = 0;
-	}
-
-	Coordinates(int longitude, int latitude, int height) {
-		_longitude = 0;
-		_latitude = 0;
-		_height = 0;
-		try {
-			setHeight(height);
-			setLatitude(latitude);
-			setLongitude(longitude);
-		} catch (Exception err)
-		{
-			System.out.println(err.getMessage());
-		}
-	}
-
-	public int		getLongitude() {return (_longitude);}
-	public int		getLatitude() {return (_latitude);}
-	public int		getHeight() {return (_height);}
-	void	setLongitude(int longitude) throws BadValueException {
-		if (longitude >= 0)
-			_longitude = longitude;
-		else
-			throw new BadValueException("Error: invalid longitude value");
-	}
-	void	setLatitude(int latitude) throws BadValueException {
-		if (latitude >= 0)
-			_latitude = latitude;
-		else
-			throw new BadValueException("Error: invalid latitude value");
-	}
-	void	setHeight(int height) throws BadValueException {
-		if (height >= 0 && height <= 100)
-			_height = height;
-		else
-			throw new BadValueException("Error: invalid height value");
-
-	}
-}
-
 class WeatherTower {
 	public String getWeather(Coordinates p_coordinates) {
-		WeatherProvider wp = WeatherProvider.getInstance();
+		WeatherProvider wp = WeatherProvider.getWeatherProvider();
 		String weather = wp.getCurrentWeather(p_coordinates);
 
 		return (weather);
@@ -89,6 +34,7 @@ class WeatherTower {
 }
 
 class Tower {
+	List<Flyable> observers;
 
 }
 
@@ -106,25 +52,4 @@ class Aircraft {
 	String _name;
 	Coordinates _coordinate;
 	Aircraft(long p_id, String p_name, Coordinates p_coordinates){}
-}
-
-
-// singletons (classe avec une seule instance)
-
-class WeatherProvider {
-	private static WeatherProvider instance = null;
-
-	private String weather[] = {"RAIN", "FOG", "SUN", "SNOW"};
-
-	private WeatherProvider() {}
-
-	public static WeatherProvider getInstance() {
-		if (instance == null)
-			instance = new WeatherProvider();
-		return (instance);
-	}
-
-	public String getCurrentWeather(Coordinates p_coordinates) {
-		return (weather[0]);
-	}
 }
